b92176ffbf70d1c1cce9818d33d3bf33b12718ac
Suhwan-Im <suwhan1992@gmail.com>
2022-04-07 22:51:02 +0900

    updating_220407

--- 4. Django/django (ModelForm)/crud/settings.py
+++ 4. Django/django (ModelForm)/crud/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-04(=c2le$#*_)gay85ge2#ixef$f7b!1^4al32i!)i!(yjz-=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
