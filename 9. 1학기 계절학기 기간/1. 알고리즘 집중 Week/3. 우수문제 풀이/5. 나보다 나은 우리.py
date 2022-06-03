df20f2c93c301b2ddd3438600f6dd7bea42b4312
Suhwan-Im <suwhan1992@gmail.com>
2022-06-03 16:30:52 +0900

    updating_220603

---
+++ 9. 1학기 계절학기 기간/1. 알고리즘 집중 Week/3. 우수문제 풀이/5. 나보다 나은 우리.py
@@ -0,0 +1,15 @@
+T = 50
+
+for tc in range(1, T+1):
+    N = int(input())
+    ans_mat = []
+    for i in range(N):
+        ans_list = str(input())
+        ans_mat.append(ans_list)
+
+
+
+
+
+    # 결과 출력
+    print(f'')
