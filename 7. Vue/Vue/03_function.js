25fb06bcea17b249c077206a56ff496699e87a94
Suhwan-Im <suwhan1992@gmail.com>
2022-06-08 02:00:00 +0900

    updating_220608

---
+++ 7. Vue/Vue/03_function.js
@@ -0,0 +1,17 @@
+const member = {
+  name: 'aiden',
+  age: '24',
+  sId: 220506001,
+}
+
+const member2 = {
+  name: 'haley',
+  age: '254',
+  sId: 220506002,
+}
+
+function Member(name, age, sId){
+  this.name = name
+  this.age = age
+  this.sId = sId
+}
