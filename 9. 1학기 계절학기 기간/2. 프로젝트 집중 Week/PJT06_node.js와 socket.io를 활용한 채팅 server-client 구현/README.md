2a283fdee43cd7e5c81832fbfb13c5acffa23790
Suhwan-Im <suwhan1992@gmail.com>
2022-06-16 23:38:38 +0900

    updating_220616

---
+++ 9. 1학기 계절학기 기간/2. 프로젝트 집중 Week/PJT06_node.js와 socket.io를 활용한 채팅 server-client 구현/README.md
@@ -0,0 +1,13 @@
+## node.js와 socket.io를 활용한 채팅 server/client 구현
+
+
+
+### 채팅창 스크린샷
+
+![image-20220616233157429](README.assets/image-20220616233157429.png)
+
+
+
+### 배운 내용
+
+node.js와 socket.io를 활용하여 간단한 채팅 프로그램을 만들어보았습니다. 비록 js 코드가 익숙하지 않아서 교재를 보고 작성했지만, index.html 파일내에 스타일(css)과 html 코드는 물론 script 탭 안에 js 코드를 작성하여 모든것을 한 파일에 구현할 수 있었습니다.
---
+++ 9. 1학기 계절학기 기간/2. 프로젝트 집중 Week/PJT06_node.js와 socket.io를 활용한 채팅 server-client 구현/server.js
@@ -0,0 +1,67 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function () {
+  console.log('server listening on port : 3000');
+});
+
+var userList = [];
+
+io.on('connection', function(socket){
+  var joinedUser = false;
+  var nickname;
+
+  // 유저 입장  
+  socket.on('join', function(data){
+    console.log(data);
+    if (joinedUser) {
+      return false;
+    }
+
+    nickname = data;
+    userList.push(nickname);
+    socket.broadcast.emit('join', {
+      nickname: nickname,
+      userList: userList
+    });
+
+    socket.emit('welcome', {
+      nickname: nickname,
+      userList: userList
+    });
+
+    joinedUser = true;
+  });
+  
+  // 메시지 전달
+  socket.on('msg', function(data){
+    console.log('msg: ' + data);
+    io.emit('msg', {
+      nickname: nickname,
+      msg: data
+    });
+  });
+
+  // 접속 종료
+  socket.on('disconnect', function () {
+    // 입장하지 않았다면 중단
+    if (!joinedUser) {
+      console.log(' -- not joinedUser left');
+      return false;
+    }
+
+    // 접속자 목록에서 제거
+    var i = userList.indexOf(nickname);
+    userList.splice(i, 1);
+
+    socket.broadcast.emit('left', {
+      nickname: nickname,
+      userList: userList
+    });
+  });
+});
