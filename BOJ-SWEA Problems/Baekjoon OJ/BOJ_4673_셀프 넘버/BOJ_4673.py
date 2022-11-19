eed9475c7914afce09513e069a32b4670a9108c1
Suhwan Im <suwhan1992@gmail.com>
2022-11-19 16:54:47 +0900

    problems_221119

--- BOJ-SWEA Problems/Baekjoon OJ/BOJ_4673_셀프 넘버/BOJ_4673.py
+++ BOJ-SWEA Problems/Baekjoon OJ/BOJ_4673_셀프 넘버/BOJ_4673.py
@@ -1,5 +1,5 @@
-num_list = list(range(1, 10001))
-fin_list = list(range(1, 10001))
+num_list = list(range(1, 10001))    # 1 부터 10000까지 순회하기 위한 숫자 리스트
+fin_list = list(range(1, 10001))    # 최종 정답만 남길 숫자 리스트
 
 for num in num_list:
     num_str = str(num)
