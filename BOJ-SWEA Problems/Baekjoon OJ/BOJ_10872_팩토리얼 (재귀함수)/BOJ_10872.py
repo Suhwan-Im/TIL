bf5cfad830800bebc32681a0713a394c5d4e61e2
Suhwan-Im <suwhan1992@gmail.com>
2022-12-10 14:50:33 +0900

    problems_221210

--- BOJ-SWEA Problems/Baekjoon OJ/BOJ_10872_팩토리얼 (재귀함수)/BOJ_10872.py
+++ BOJ-SWEA Problems/Baekjoon OJ/BOJ_10872_팩토리얼 (재귀함수)/BOJ_10872.py
@@ -1,9 +1,10 @@
 N = 10
 
-def factorial(num):
-    if num <= 1:
+# 재귀함수를 이용한 팩토리얼 함수
+def factorial(n):
+    if n <= 1:
         return 1
     else:
-        return num * factorial(num-1)
+        return n * factorial(n-1)
 
 print(factorial(N))
